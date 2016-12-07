08e4b541a1a9cbda2d9c430ce70eaa86e419ae60
William Beard <wbeard@rallydev.com>
2016-12-06 21:17:34 -0700

    Improve basic example

--- basic/basic.js
+++ basic/basic.js
@@ -1,28 +1,34 @@
 // @flow
 import React, { Component } from 'react';
 
+type Animal = 'bird' | 'cat' | 'dog';
 type Possession = {
   name: string,
   value: number
+};
+type Person = {
+  name: string
 };
 type Props = {
   alive: boolean,
   age: number,
   name: string,
-  love?: any,
-  friends: Array<Person> | null,
+  love: any,
+  friends: Array<Person | Animal>,
   possessions: Array<Possession>,
-  homePlanet: 'Earth' | 'Mars'
+  homePlanet?: 'Earth' | 'Mars'
 };
 
-export default class Person extends Component {
-  props: Props;
-  
-  render() {
-    return (
-      <div>
-        { this.props.name }
-      </div>
-    );
-  }
+function PersonComponent(props: Props) {
+  return (
+    <div>
+      <span>Is Alive: { props.alive }</span>
+      <span>Age: { props.age }</span>
+      <span>Name: { props.mane }</span>
+      <span>Loves: { props.love }</span>
+      <span>Friends: <ul>{ props.friends.map(friend => <li>friend</li>) }</ul></span>
+      <span>Friends: <ul>{ props.possessions.map(pos => <li>pos.name</li>) }</ul></span>
+      <span>Home Planet: { props.homePlanet ? props.homePlanet : 'None' }</span>
+    </div>
+  );
 };
